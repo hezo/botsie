8c77cdfbe38e91399fd90f55c63a24829a81e170
Quh <iamquh@gmail.com>
2013-02-23 22:40:55 +0200

    Added support for module reloading, added a !help command

--- bot.js
+++ bot.js
@@ -6,78 +6,103 @@ var request = require('request');
  * Only some of these config are used...
  */
 var config = {
-	channels : ["#finnexus"],
-	server : "irc.quakenet.org",
-	botName : "Seilori",
-	userName : 'nodebot',
-	realName : 'nodeJS IRC client',
-	port : 6667,
-	debug : true,
-	showErrors : false,
-	autoRejoin : true,
-	autoConnect : true,
-	secure : false,
-	selfSigned : false,
-	certExpired : false,
-	floodProtection : false,
-	floodProtectionDelay : 1000,
-	stripColors : false,
-	channelPrefixes : "&#",
-	messageSplit : 512
+    channels:["#quhbottest"],
+    server:"irc.quakenet.org",
+    botName:"SexRowBot",
+    userName:'nodebot',
+    realName:'nodeJS IRC client',
+    port:6667,
+    debug:true,
+    showErrors:false,
+    autoRejoin:true,
+    autoConnect:true,
+    secure:false,
+    selfSigned:false,
+    certExpired:false,
+    floodProtection:false,
+    floodProtectionDelay:1000,
+    stripColors:false,
+    channelPrefixes:"&#",
+    messageSplit:512
 };
 
 var bot = new irc.Client(config.server, config.botName, {
-	channels : config.channels,
-	userName : 'seilori',
-	realName : 'Pekka Merimies',
-	autoRejoin : true,
-	autoConnect : true,
-	floodProtection : true,
-	floodProtectionDelay : 1000,
-	showErrors : true
+    channels:config.channels,
+    userName:'seilori',
+    realName:'Pekka Merimies',
+    autoRejoin:true,
+    autoConnect:true,
+    floodProtection:true,
+    floodProtectionDelay:1000,
+    showErrors:true
 });
 
 
-bot.addListener("join", function(channel, who) {
-	// Welcome them in!
-	if (who == "hezo") {
-		bot.say(channel, who + " ooh its me clown!");
-	}
-	if (who == "Quh") {
-		bot.say(channel, who + ": my master!");
-	}
+bot.addListener("join", function (channel, who) {
+    // Welcome them in!
+    if (who == "hezo") {
+        bot.say(channel, who + " ooh its me clown!");
+    }
+    if (who == "Quh") {
+        bot.say(channel, who + ": my master!");
+    }
 });
 
-bot.addListener('error', function(message) {
-	console.log('error: ', message);
+bot.addListener('error', function (message) {
+    console.log('error: ', message);
 });
 
 //Load dem modules.
 var fs = require("fs");
 var path = require('path');
 
-var modules = {};
-console.log("Modules loaded:");
-fs.readdirSync("./botmodules").forEach(function(filename) {
-    if(filename != 'index.js' && path.extname(filename) == ".js")
-    {
-        modules[path.basename(filename, ".js")] = require("./botmodules/"+filename);
-        console.log(filename +" : "+ path.basename(filename, ".js"));
-    }
-});
+var modules = {}
+var commands = "";
+reloadModules();
 
-bot.addListener('message', function(from, to, message) {
+bot.addListener('message', function (from, to, message) {
     if (message.substring(0, 1) == "!") {
         splitted = message.split(" ");
         askedmodule = splitted[0].substring(1);
-        modargs = message.substring(askedmodule.length+2);
-        if(typeof modules[askedmodule] !== "undefined")
-       		modules[askedmodule].modexec(to, bot, modargs);
+        modargs = message.substring(askedmodule.length + 2);
+
+        switch (askedmodule) {
+            case "help":
+                listModules(to);
+                break;
+            case "reload":
+                reloadModules(to);
+                break;
+        }
+
+        if (typeof modules[askedmodule] !== "undefined")
+            modules[askedmodule].modexec(to, bot, modargs);
     }
 });
 
+function listModules(to) {
+    bot.say(to, "Supported commands: " + commands);
+}
+
+
+function reloadModules(to) {
+    modules = {};
+    commands = "";
+    console.log("Modules loaded:");
+    fs.readdirSync("./botmodules").forEach(function (filename) {
+        if (filename != 'index.js' && path.extname(filename) == ".js") {
+            modules[path.basename(filename, ".js")] = require("./botmodules/" + filename);
+            commands += "!" + path.basename(filename, ".js") + ", ";
+            console.log(filename + " : " + path.basename(filename, ".js"));
+        }
+    });
+
+    commands = commands.substr(0,commands.length-2);
+    if (to) bot.say(to, "Modules reloaded!");
+}
+
 //do some error handling magic here!
 //and this is bad place to be
-process.on('uncaughtException', function(err) {
-	console.log(err);
+process.on('uncaughtException', function (err) {
+    console.log(err);
 });

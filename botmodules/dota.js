8e8eb48ea433fdbb6fd377e191fc41cc5662e5a2
Defu <hello@world.com>
2013-02-27 17:05:09 +0200

    Dota 2

--- botmodules/dota.js
+++ botmodules/dota.js
@@ -1,5 +1,28 @@
 var request = require("request");
 
-exports.modexec =  function(to, bot,playerName) {
-    bot.say(to, "Error Error. "+playerName);
-};
+exports.modexec =  function(to, bot, modargs) {
+    splitparams = modargs.split(" ");
+
+    if (splitparams.length > 2) {
+        bot.say(to, "Error Error. Nyt jumalauta");
+    }
+	else if (splitparams.length < 2) {
+		playerStats(to, bot, splitparams)
+	}
+	else { 
+		requestType(to, bot, splitparams)
+	}
+}
+	
+	
+function getUrl(type) {
+	
+	var base = "http://api.steampowered.com/IDOTA2Match_570/";
+	
+}	
+	
+function requestType(to, bot, params) {
+	
+	
+	
+}

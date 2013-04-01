78ef57305583a7fded63576374c52c73a4de3c02
hezo <spam@id-idle.eu>
2013-04-01 22:55:41 +0300

    try / catch

--- botmodules/w.js
+++ botmodules/w.js
@@ -20,43 +20,49 @@ exports.modexec = function(to, bot, place) {
 	weatherUrl += "&place="+place;
 	var message = place+": ";
 	request(weatherUrl, function(error, response, body) {
+	//console.log(body);
 		if (!error && response.statusCode == 200) {
 			parseString(body, function (err, result) {
-				var tempArray = result["wfs:FeatureCollection"]["wfs:member"][0]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
-				var lastTemp = tempArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
-				
-				var windArray = result["wfs:FeatureCollection"]["wfs:member"][1]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
-				var lastWind = windArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
-				
-				var airPressureArray = result["wfs:FeatureCollection"]["wfs:member"][9]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
-				var lastAirPressure = airPressureArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
-				
-				var rainArray = result["wfs:FeatureCollection"]["wfs:member"][7]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
-				var lastRain = rainArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
-				
-				var snowArray = result["wfs:FeatureCollection"]["wfs:member"][8]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
-				var snow = snowArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
-				
-				if(lastTemp !== 'NaN') {
-					message += lastTemp +"c";
+			console.log(result);
+				try {
+					var tempArray = result["wfs:FeatureCollection"]["wfs:member"][0]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
+					var lastTemp = tempArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
+					
+					var windArray = result["wfs:FeatureCollection"]["wfs:member"][1]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
+					var lastWind = windArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
+					
+					var airPressureArray = result["wfs:FeatureCollection"]["wfs:member"][9]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
+					var lastAirPressure = airPressureArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
+					
+					var rainArray = result["wfs:FeatureCollection"]["wfs:member"][7]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
+					var lastRain = rainArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
+					
+					var snowArray = result["wfs:FeatureCollection"]["wfs:member"][8]["omso:PointTimeSeriesObservation"][0]["om:result"][0]["wml2:MeasurementTimeseries"][0]["wml2:point"];
+					var snow = snowArray[tempArray.length-1]["wml2:MeasurementTVP"][0]["wml2:value"][0];
+					
+					if(lastTemp !== 'NaN') {
+						message += lastTemp +"c";
+					}
+					
+					if(lastWind !== 'NaN') {
+						message += ", Wind: "+lastWind+"m/s";
+					}
+					
+					if(lastAirPressure !== 'NaN') {
+						message += ", Air pressure: "+lastAirPressure+"hPa";
+					}
+					
+					if(lastRain !== 'NaN' && lastRain !== "0.0") {
+						message += ", Rain: "+lastRain+"mm/h";
+					}
+					
+					if(snow !== 'NaN' && snow !== "0.0") {
+						message += ", Snow level: "+snow+"cm";
+					}
+				} catch(err) {
+					console.error(err);
+					message += "No Data";
 				}
-				
-				if(lastWind !== 'NaN') {
-					message += ", Wind: "+lastWind+"m/s";
-				}
-				
-				if(lastAirPressure !== 'NaN') {
-					message += ", Air pressure: "+lastAirPressure+"hPa";
-				}
-				
-				if(lastRain !== 'NaN' && lastRain !== "0.0") {
-					message += ", Rain: "+lastRain+"mm/h";
-				}
-				
-				if(snow !== 'NaN' && snow !== "0.0") {
-					message += ", Snow level: "+snow+"cm";
-				}
-				
 				bot.say(to, message);
 				console.log("Weather: %s", message);
 			});

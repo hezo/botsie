48bdb661661004c9f7013b416b184111799acd27
hezo <spam@id-idle.eu>
2013-05-05 21:23:17 +0300

    paths

--- botmodules/url.js
+++ botmodules/url.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose');
 var nconf = require('nconf');
 
 nconf.argv().env().file({ file: './config/mongo.json' });
-mongoose.connect(nconf.get('uri'));
+var uri = nconf.get('uri');
+mongoose.connect(uri);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));

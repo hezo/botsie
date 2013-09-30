cdd872a1234e596e13d6532f6b8b341109019c43
Hezo <Hezo@Spitfire>
2013-09-30 22:29:50 +0300

    Added little nicer botsie url snatcher page

--- bot.js
+++ bot.js
@@ -17,80 +17,87 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 var botConfigs = nconf.get('bots');
 var modulesDir = nconf.get('modules');
+var mode = nconf.get('mode');
 var bot = {};
 
-for(config in botConfigs) {
-	bot[config] = new irc.Client(botConfigs[config].server, botConfigs[config].botName, {
-	    channels:botConfigs[config].channels,
-	    userName:botConfigs[config].userName,
-	    realName:botConfigs[config].realName,
-	    autoRejoin:botConfigs[config].autoRejoin,
-	    autoConnect:botConfigs[config].autoConnect,
-	    floodProtection:botConfigs[config].floodProtection,
-	    floodProtectionDelay:botConfigs[config].floodProtectionDelay,
-	    showErrors:botConfigs[config].showErrors,
-	    debug: false
-	});
-	
-	bot[config].addListener('error', function (message) {
-	    console.log('error: ', message);
-	});
-	
-	bot[config].botname = config;
-	bot[config].modules = {};
-	bot[config].modulesCommands = "";
-	bot[config].modulesDir = modulesDir;
-	bot[config].loadModules = function() {
-	    fs.readdirSync(this.modulesDir).forEach(function (filename) {
-	        if (filename != 'index.js' && path.extname(filename) == ".js") {
-	            bot[config].modules[path.basename(filename, ".js")] = require(modulesDir+"/"+filename);
-	            bot[config].modulesCommands += "!" + path.basename(filename, ".js") + ", ";
-	            console.log(filename + " : " + path.basename(filename, ".js"));
-	        }
-	    });
-	
-		//init if module has init() function
-	    for(mod in bot[config].modules) {
-	    	if (typeof bot[config].modules[mod].init === "function") {
-	    		console.log("init(): "+mod);
-	    		bot[config].modules[mod].init(this);
-	    	}
-	    };
-	
-	    bot[config].modulesCommands = bot[config].modulesCommands.substr(0,bot[config].modulesCommands.length-2);
-	};
-	bot[config].listModules = function(to) {
-		bot[config].say(to, "Supported commands: " + this.commands);
-	};
-	//load modules
-	bot[config].addListener('message', function (from, to, message, gg) {
-	    if (message.substring(0, 1) == "!") {
-	        splitted = message.split(" ");
-	        askedmodule = splitted[0].substring(1);
-	        modargs = message.substring(askedmodule.length + 2);
-	
-	        switch (askedmodule) {
-	            case "help":
-	                bot[config].listModules(to);
-	                break;
-	        }
-	
-	        if (typeof bot[config].modules[askedmodule] !== "undefined")
-	            bot[config].modules[askedmodule].modexec(to, this, modargs);
-	    }
-	    
-	    var logging = new loggingModel({ from: from, to:to, message: message, date: Date.now(), bot: config});
-		logging.save(function (err, mUrl) {
-			if (err) {
-				console.log(err);
-			}
+switch (nconf.get('mode')) {
+case "http":
+	http.init(bot);
+	break;
+default:
+	for(config in botConfigs) {
+		bot[config] = new irc.Client(botConfigs[config].server, botConfigs[config].botName, {
+		    channels:botConfigs[config].channels,
+		    userName:botConfigs[config].userName,
+		    realName:botConfigs[config].realName,
+		    autoRejoin:botConfigs[config].autoRejoin,
+		    autoConnect:botConfigs[config].autoConnect,
+		    floodProtection:botConfigs[config].floodProtection,
+		    floodProtectionDelay:botConfigs[config].floodProtectionDelay,
+		    showErrors:botConfigs[config].showErrors,
+		    debug: false
 		});
-	    
-	});
-	bot[config].loadModules();
+		
+		bot[config].addListener('error', function (message) {
+		    console.log('error: ', message);
+		});
+		
+		bot[config].botname = config;
+		bot[config].modules = {};
+		bot[config].modulesCommands = "";
+		bot[config].modulesDir = modulesDir;
+		bot[config].loadModules = function() {
+		    fs.readdirSync(this.modulesDir).forEach(function (filename) {
+		        if (filename != 'index.js' && path.extname(filename) == ".js") {
+		            bot[config].modules[path.basename(filename, ".js")] = require(modulesDir+"/"+filename);
+		            bot[config].modulesCommands += "!" + path.basename(filename, ".js") + ", ";
+		            console.log(filename + " : " + path.basename(filename, ".js"));
+		        }
+		    });
+		
+			//init if module has init() function
+		    for(mod in bot[config].modules) {
+		    	if (typeof bot[config].modules[mod].init === "function") {
+		    		console.log("init(): "+mod);
+		    		bot[config].modules[mod].init(this);
+		    	}
+		    };
+		
+		    bot[config].modulesCommands = bot[config].modulesCommands.substr(0,bot[config].modulesCommands.length-2);
+		};
+		bot[config].listModules = function(to) {
+			bot[config].say(to, "Supported commands: " + this.commands);
+		};
+		//load modules
+		bot[config].addListener('message', function (from, to, message, gg) {
+		    if (message.substring(0, 1) == "!") {
+		        splitted = message.split(" ");
+		        askedmodule = splitted[0].substring(1);
+		        modargs = message.substring(askedmodule.length + 2);
+		
+		        switch (askedmodule) {
+		            case "help":
+		                bot[config].listModules(to);
+		                break;
+		        }
+		
+		        if (typeof bot[config].modules[askedmodule] !== "undefined")
+		            bot[config].modules[askedmodule].modexec(to, this, modargs);
+		    }
+		    
+		    var logging = new loggingModel({ from: from, to:to, message: message, date: Date.now(), bot: config});
+			logging.save(function (err, mUrl) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		    
+		});
+		bot[config].loadModules();
+	}
+	http.init(bot);
+	break;
 }
-
-http.init(bot);
 
 //do some error handling magic here!
 //and this is bad place to be
--- http/static/js/url.js
+++ http/static/js/url.js
@@ -1,11 +1,14 @@
-var app = angular.module('url', []).
-	config(function($routeProvider) {
-		$routeProvider.
-			when('/', {controller:MainCtrl, templateUrl:'views/list.html'}).
-			otherwise({redirectTo:'/'});
+var app = angular.module('url', []).config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller : MainCtrl,
+		templateUrl : 'views/list.html'
+	}).otherwise({
+		redirectTo : '/'
 	});
-	function MainCtrl($scope, $http) {
-		$http.get("/url/all").success(function(data){
-			$scope.urls = data;
-		});	
-	}
+});
+
+function MainCtrl($scope, $http) {
+	$http.get("/url/all").success(function(data) {
+		$scope.urls = data;
+	});
+};

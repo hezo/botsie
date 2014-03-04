42d49ee628c1014b6aa61bac98836ee0f77e25ab
Hezo <Hezo@Spitfire>
2014-03-04 16:45:33 +0200

    Added load indicator to urls

--- http/static/js/app/url/urlController.js
+++ http/static/js/app/url/urlController.js
@@ -1,11 +1,11 @@
 var UrlController = app.controller("UrlController", function($scope, $log, urls) {
 	$log.log("Url controller");
 	$scope.urls = urls;
+	
 });
 
-
 UrlController.resolve = {
-		urls: function(UrlService) {
+		urls: function(UrlService, $log) {
 			return UrlService.query();
 		}
 };
